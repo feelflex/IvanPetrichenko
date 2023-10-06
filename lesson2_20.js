ae48ee2788a6deaac530cdab4683d73d787e03a2
Sergejs <114947877+feelflex@users.noreply.github.com>
2023-10-06 10:22:35 +0200

    Update lesson2_20.js
    Some changes

--- lesson2_20.js
+++ lesson2_20.js
@@ -2,10 +2,10 @@ const quantityHamburger = 10;
 const quantityFries = 3;
 
 const hamburger = prompt("How many hamburgers have you had?", "");
-const fries = prompt("How many hamburgers have you had?", "");
+const fries = prompt("How many fries have you had?", "");
 
 const hamburger2 = prompt("How many hamburgers have you had?", "");
-const fries2 = prompt("How many hamburgers have you had?", "");
+const fries2 = prompt("How many fries have you had?", "");
 
 const friend1 = parseInt(hamburger) + parseInt(fries);
 const friend2 = parseInt(hamburger2) + parseInt(fries2);
